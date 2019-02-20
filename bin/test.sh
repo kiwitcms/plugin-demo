bf75d0abe9695e3ee3b49b8944bab9db43bb25e6
Mr. Senko <atodorov@mrsenko.com>
2019-02-21 00:25:00 +0200

    Configure TCMS_BUILD to be commit hash instead of TRAVIS_BUILD_NUMBER
    shorten the hash to the first 7 digits

--- bin/test.sh
+++ bin/test.sh
@@ -3,6 +3,7 @@
 # Execute this from the project root directory
 
 export TCMS_PRODUCT_VERSION=$TRAVIS_BRANCH
+export TCMS_BUILD=`echo $TRAVIS_COMMIT | cut -c1-7`
 
 nosetests --with-tap --tap-stream 2> output.tap
 tcms-tap-plugin output.tap
