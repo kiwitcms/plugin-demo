e484e59023caf665e8f93341395af6f397691b93
Mr. Senko <atodorov@mrsenko.com>
2019-02-21 00:00:50 +0200

    Configure TCMS_PRODUCT_VERSION == master
    so all results get reported under the same TestPlan from now on

--- bin/test.sh
+++ bin/test.sh
@@ -2,6 +2,10 @@
 
 # Execute this from the project root directory
 
+# we want Version == "master" so that all results get reported under
+# the same TestPlan
+export TCMS_PRODUCT_VERSION="master"
+
 nosetests --with-tap --tap-stream 2> output.tap
 tcms-tap-plugin output.tap
 
